25e29271e86abdf5a9c5a6b802388384361d2ac0
aong13 <oaoa13@pukyong.ac.kr>
2024-07-23 22:05:59 +0900

    feat: 검색 페이지

---
+++ src/components/BasicHeader.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const back_icon = require('../assets/icons/back.png');
+
+const BasicHeader = ({ title, showBackButton = true, rightButtons = [] }) => {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.headerWrapper}>
+            {showBackButton && (
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Image source={back_icon} style={styles.headerIcon} />
+                </TouchableOpacity>
+            )}
+            <Text style={styles.headerTitle}>{title}</Text>
+            <View style={styles.headerButtonsWrapper}>
+                {rightButtons.map((button, index) => (
+                    <TouchableOpacity key={index} onPress={button.onPress||""}> 
+                        <Image source={button.icon} style={styles.headerIcon} />
+                    </TouchableOpacity>
+                ))}
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerWrapper: {
+        backgroundColor: '#FFF',
+        paddingHorizontal: 16,
+        paddingVertical: 18,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#EAEAEA',
+    },
+    headerTitle: {
+        position:'absolute',
+        left: '50%',
+        fontSize: 16,
+        fontWeight: 'bold',
+        lineHeight: 19.97,
+        textAlign: 'center',
+        color: '#000',
+    },
+    headerButtonsWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
+    },
+    headerIcon:{
+        width: 28,
+        height: 28
+    },
+});
+
+export default BasicHeader;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,142 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, Pressable, ScrollView, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import BasicHeader from '../components/BasicHeader';
+import { siObj, siGuList } from '../data/Si-Gun-goo';
 
-const Search = () => {
-  return (
-    <SafeAreaView>
-      <Text>Search</Text>
-    </SafeAreaView>
-  )
+const Search = ({ navigation }) => {
+    const { top, bottom } = useSafeAreaInsets();
+    const [selectSi, setSelectSi] = useState('');
+    const [selectDo, setSelectDo] = useState('');
+    const [selectDong, setSelectDong] = useState('');
+    const [doList, setDoList] = useState([]);
+    const [selectDongList, setSelectDongList] = useState([]);
+
+    // 시 선택 시
+    const handleTouchSi = (e) => {
+        setSelectSi(e);
+        const selectedSi = e.long;
+        const selectedGu = siGuList.find(item => item.si === selectedSi);
+        setDoList(selectedGu ? selectedGu.gu : []);
+        setSelectDo('');
+        setSelectDongList([]);
+    };
+
+    // 구 선택 시
+    const handleTouchGu = (e) => {
+        setSelectDo(e);
+        const selectedDong = selectDongList.filter(item => item.includes(e));
+        setSelectDongList(selectedDong);
+    };
+
+    // 동 선택 시
+    const handleTouchDong = (e) => {
+        navigation.navigate('SelectDongResult', { address: e });
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF', paddingTop: top }}>
+            <BasicHeader title={'동 검색'} />
+            <View style={{ flexDirection: 'row', flex: 1 }}>
+                <View style={{ flex: 0.2, backgroundColor: '#F5F5F5' }}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        {siObj.map((e, i) => (
+                            <Pressable 
+                                key={i} 
+                                style={e.short === selectSi.short ? styles.onSelectSi : styles.offSelectSi} 
+                                onPress={() => handleTouchSi(e)}
+                            >
+                                <Text allowFontScaling={false} style={e.short === selectSi.short ? styles.onSelectSiText : styles.offSelectSiText}>
+                                    {e.short}
+                                </Text>
+                            </Pressable>
+                        ))}
+                        <View style={{ height: bottom }} />
+                    </ScrollView>
+                </View>
+                <View style={{ flex: 0.4, borderRightWidth: 0.5, borderColor: '#C3C3C3' }}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        {doList.map((e, i) => (
+                            <Pressable 
+                                style={styles.DoWrapper} 
+                                key={i} 
+                                onPress={() => handleTouchGu(e)}
+                            >
+                                <Text allowFontScaling={false} style={e === selectDo ? styles.onSelectDo : styles.offSelectDo}>
+                                    {e}
+                                </Text>
+                            </Pressable>
+                        ))}
+                        <View style={{ height: bottom }} />
+                    </ScrollView>
+                </View>
+                <View style={{ flex: 0.4, borderRightWidth: 0.5, borderColor: '#C3C3C3' }}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        {selectDongList.map((e, i) => (
+                            <Pressable 
+                                onPress={() => handleTouchDong(e)} 
+                                style={styles.DoWrapper} 
+                                key={i}
+                            >
+                                <Text allowFontScaling={false} style={e === selectDong ? styles.onSelectDo : styles.offSelectDo}>
+                                    {e.split(' ')[2]}
+                                </Text>
+                            </Pressable>
+                        ))}
+                        <View style={{ height: bottom }} />
+                    </ScrollView>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
 
-export default Search
+const styles = StyleSheet.create({
+    onSelectSi: {
+        backgroundColor: '#4AABFF',
+        paddingVertical: 16,
+        paddingHorizontal: 24,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#C3C3C3',
+    },
+    offSelectSi: {
+        backgroundColor: '#F5F5F5',
+        paddingVertical: 16,
+        paddingHorizontal: 24,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#C3C3C3',
+    },
+    onSelectSiText: {
+        fontSize: 16,
+        lineHeight: 20,
+        color: '#FFF',
+        textAlign: 'center',
+    },
+    offSelectSiText: {
+        fontSize: 14,
+        lineHeight: 20,
+        color: '#888',
+        textAlign: 'center',
+    },
+    DoWrapper: {
+        paddingVertical: 16,
+        paddingHorizontal: 0,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#C3C3C3',
+        marginHorizontal: 16,
+    },
+    onSelectDo: {
+        fontSize: 16,
+        lineHeight: 20,
+        color: '#4AABFF',
+        textAlign: 'center',
+    },
+    offSelectDo: {
+        fontSize: 14,
+        lineHeight: 20,
+        color: '#888',
+        textAlign: 'center',
+    },
+});
+
+export default Search;
